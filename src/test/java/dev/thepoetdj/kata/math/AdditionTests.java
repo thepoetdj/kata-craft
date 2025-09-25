236a57139a2f7a6d4cbaaaeaac9a33185ac72e35
Dhruv Joshi <thepoetdj@gmail.com>
2025-09-25 22:21:14 +0530

    expect failure when numbers are provided

--- src/test/java/dev/thepoetdj/kata/math/AdditionTests.java
+++ src/test/java/dev/thepoetdj/kata/math/AdditionTests.java
@@ -3,6 +3,7 @@ package dev.thepoetdj.kata.math;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.CsvSources;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,7 +16,11 @@ public class AdditionTests {
     }
 
     @ParameterizedTest
-    @CsvSource("'',0")
+    @CsvSources({
+            @CsvSource("'',0"),
+            @CsvSource("'1',1"),
+            @CsvSource("'1,5',6"),
+    })
     void addNumbersSeparatedByComma(String numbers, int result) {
         assertEquals(result, sut.add(numbers));
     }

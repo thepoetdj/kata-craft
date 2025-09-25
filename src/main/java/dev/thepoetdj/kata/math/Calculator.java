7f381b27189025cb52627046c0865bde177cbebc
Dhruv Joshi <thepoetdj@gmail.com>
2025-09-25 22:35:43 +0530

    add numbers split at default separator

--- src/main/java/dev/thepoetdj/kata/math/Calculator.java
+++ src/main/java/dev/thepoetdj/kata/math/Calculator.java
@@ -1,7 +1,14 @@
 package dev.thepoetdj.kata.math;
 
+import java.util.Arrays;
+
 public class Calculator {
+    private static final String DEFAULT_SEPARATOR = ",";
+
     public int add(String numbers) {
-        return 0;
+        if (numbers.isBlank()) return 0;
+        return Arrays.stream(numbers.split(DEFAULT_SEPARATOR))
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 }

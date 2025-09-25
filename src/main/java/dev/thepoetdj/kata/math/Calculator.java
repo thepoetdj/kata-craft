4d4235c8930a91147120bbc895723e7bfe33e3c3
Dhruv Joshi <thepoetdj@gmail.com>
2025-09-25 23:36:50 +0530

    allow to split numbers string on custom delimiter

--- src/main/java/dev/thepoetdj/kata/math/Calculator.java
+++ src/main/java/dev/thepoetdj/kata/math/Calculator.java
@@ -1,14 +1,28 @@
 package dev.thepoetdj.kata.math;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
 
 public class Calculator {
-    private static final String DEFAULT_SEPARATOR = "[\n,]";
-
     public int add(String numbers) {
         if (numbers.isBlank()) return 0;
-        return Arrays.stream(numbers.split(DEFAULT_SEPARATOR))
-                .mapToInt(Integer::parseInt)
-                .sum();
+        return toNumeric(numbers).sum();
+    }
+
+    private IntStream toNumeric(String numbers) {
+        return Arrays.stream(splitter(numbers)).mapToInt(Integer::parseInt);
+    }
+
+    private String[] splitter(String input) {
+        String delimiter = "[\n,]";
+        Matcher customDelimiter = Pattern.compile("//(.)\\R(.*)", Pattern.DOTALL)
+                .matcher(input);
+        if (customDelimiter.matches()) {
+            delimiter = customDelimiter.group(1);
+            return customDelimiter.group(2).split(delimiter);
+        }
+        return input.split(delimiter);
     }
 }
